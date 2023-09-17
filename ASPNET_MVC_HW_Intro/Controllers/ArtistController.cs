221b9bb2301f7c12a36b0b7631e28620a33db128
KapitanyukMax <maxik20192006max@gmail.com>
2023-09-17 12:12:02 +0300

    Add project files.

---
+++ ASPNET_MVC_HW_Intro/Controllers/ArtistController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_MVC_HW_Intro.Controllers
+{
+    public class ArtistController : Controller
+    {
+        public IActionResult General()
+        {
+            return View();
+        }
+
+        public IActionResult Biography()
+        {
+            return View();
+        }
+
+        public IActionResult Pictures()
+        {
+            return View();
+        }
+    }
+}
